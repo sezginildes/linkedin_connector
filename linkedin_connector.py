5360a903b1b62ca51c59ff2cb3384b75740638dd
talha002 <talha002@gmail.com>
2023-10-15 17:00:58 +0300

    Chatgpt feature is added,  message bubble bug fixed and some addtional parts added to connection function for avoiding bugs.

--- linkedin_connector.py
+++ linkedin_connector.py
@@ -1,3 +1,11 @@
 import linkedin_connector_functions as lcf
 
-lcf.connector_pipe_line(url="https://www.linkedin.com/search/results/people/?keywords=it%20recruiter&origin=SWITCH_SEARCH_VERTICAL&searchId=466b5b75-ad6a-43bd-a47a-cd04f58be04a&sid=M9", e_mail='talha002@gmail.com', password='Hawkskanks123.', PATH='chromedriver.exe', message='Hello, I work as a data scientist/AI developer in cyber security sector. I would like to connect with you.')
+lcf.connector_pipe_line(
+    login_link="https://www.linkedin.com/comm/checkpoint/lg/otp-submit/flagship?sig=05tbiKv7sMGGY1&submissionId=AgENaUfwq1xLAgAAAYszcIMGHKjQTCvRl-GzBZpyD9kEVQsaqIHDZWaHw2bF9vNlCZkU2ux2Ocdgw_LAzp3SxwaGJVWSHIrTk05e7g&loginToken=AQHIIr_K6lPodgAAAYszcIOnr2iaLGORloZm71SH2ZGRVDlYXqooHjytEABe_UbHOvHD4K-nK8ossHS_UL0Vd5NAm00KCW0wHQbxJ60MaRUM4A_XxyI&session_redirect=%2Ffeed%2F%3FparentPageKey%3Dd_request-otp-generation&fromSignIn=true&midToken=AQEAqebx11W4jw&midSig=2gb2R-m7EMGGY1&trk=eml-email_security_one_time_sign_in_link_checkpoint-null-1-null&trkEmail=eml-email_security_one_time_sign_in_link_checkpoint-null-1-null-null-c6ljey%7Elnrhb0e5%7E9m-null-neptune%2Fcheckpoint%7Eotp%7Esubmit%7Eflagship&lipi=urn%3Ali%3Apage%3Aemail_email_security_one_time_sign_in_link_checkpoint%3BK%2FljdyiISAe47dB3fLih%2FA%3D%3D",
+    search_link="https://www.linkedin.com/search/results/people/?geoUrn=%5B%22101282230%22%5D&keywords=data%20scientist&origin=FACETED_SEARCH&searchId=498aa9be-2ccb-4a87-a5ac-8d1ce11f8b42&sid=klL", 
+    PATH='chromedriver.exe', 
+    message='chatgpt', 
+    my_informations="\n- Working as Data Scientist\n- Doing masters at Big Data\n- Doing traineeship at BME\n- My name: Talha",
+    openai_key="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",
+    limit=100,
+    headless=False)
--- linkedin_connector_functions.py
+++ linkedin_connector_functions.py
@@ -1,8 +1,74 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
 import time
+import openai
+from openai.error import ServiceUnavailableError
+
+def chat_with_chatgpt(messages, key):
+    '''
+    Function for sending prompt to chatgpt.
+
+    key: str (representing the openai API key for sending requests to chatgpt.)
+    messeges: str (representing the prompt sending to chatgpt)
+    '''
+
+    openai.api_key = key
+    completion = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[{'role': 'user', 'content': messages}],
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    answer = completion['choices'][0]['message']['content']
+    return answer
+
+def anlyze_profile(driver, peep_element, informations, key):
+    '''
+    Function for analysing linkedin profiles and sending them to chatgpt.
+
+    driver: Selenium WebDriver object (represents the web browser that you want to control and automate).
+    peep_element: Selenium WebDriver object (represents the webdriver element which includes profile link).
+    key: str (representing the openai API key for sending requests to chatgpt.)
+    informations: str (representing the career information of user)
+    '''
+
+    peep_profile = peep_element.find_element(By.TAG_NAME, 'a').get_attribute("href")
+    driver.execute_script("window.open(arguments[0])", peep_profile)
+    driver.switch_to.window(driver.window_handles[-1])
+
+    WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CLASS_NAME, 'pv-text-details__left-panel')))
+
+    header = driver.find_element(By.CLASS_NAME, 'pv-text-details__left-panel').text
+    try:
+        WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '//section[.//div[@id="about"]]')))
+        about_section = driver.find_element(By.XPATH, '//section[.//div[@id="about"]]').text
+    except TimeoutException:
+        about_section = "None"
+    try:
+        experience = driver.find_element(By.XPATH, '//section[.//div[@id="experience"]]').text
+    except:
+        experience = "None"
+
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
+
+    prompt = f"I want to send an introduction message limited to 300 characters to a person from linkedin. Write me an introduction message in 300 characters based on the information I gave down below:\nMy information:{informations}\nInformation of the person:\nName&Summary\n{header}\n{about_section}\n{experience}\nDont Metnion: collabration, hashtags."
+
+    while True:
+        try:
+            answer = chat_with_chatgpt(messages=prompt, key=key)
+            break
+        except ServiceUnavailableError:
+            print('The Chatgpt server is overloaded or not ready yet. Tryin again in 5 seconds.')
+
+    return answer
 
 def login(driver, e_mail, password):
     '''
@@ -23,12 +89,14 @@ def login(driver, e_mail, password):
 
     password_input.submit()
 
-def connect(driver, message=False):
+def connect(driver, message=False, openai_key="None", my_informations="None"):
     '''
     Function to send connection invite
 
     driver: Selenium WebDriver object (Represents the web browser that you want to control and automate.)
-    message (optional): srt, optional (Allows you to provide a custom message to send along with the connection request. If not provided, it defaults to False.)
+    message (optional): str, optional (Allows you to provide a custom message to send along with the connection request. If not provided, it defaults to False. If it's defined as chatgpt it will send a message based on the profile of target.)
+    openai_key (optional): str, optional (Needed api key for chatgpt function.)
+    my_informations (optional): str, optional (Your Linkedin profile informations, needed for sending targeted notes while connecting)
     '''
     WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'li[class="reusable-search__result-container"]')))
     driver.execute_script(f"window.scrollTo(0, document.body.scrollHeight)")
@@ -44,18 +112,41 @@ def connect(driver, message=False):
 
     for peep in people:
         connect_btn = peep.find_element(By.TAG_NAME, 'button')
+        
         if connect_btn.text == "Connect":
+
+            if message == "chatgpt":
+                    answer = anlyze_profile(driver, peep, informations=my_informations, key=openai_key)
+
+            message_bubbles = driver.find_elements(By.CSS_SELECTOR, 'div[aria-label="Messaging"]')
+            for bubble in message_bubbles:
+                close = bubble.find_element(By.TAG_NAME, "button")
+                close.click()
+
             connect_btn.click()
 
             try:
-                if message:
+                if message == "chatgpt":
                     note_btn = driver.find_element(By.CSS_SELECTOR, 'button[aria-label="Add a note"]')
                     note_btn.click()
+                    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'textarea[name="message"]')))
+                    text_area = driver.find_element(By.CSS_SELECTOR, 'textarea[name="message"]')
+                    text_area.send_keys(answer)
+                    time.sleep(2)
+                elif message:
+                    note_btn = driver.find_element(By.CSS_SELECTOR, 'button[aria-label="Add a note"]')
+                    note_btn.click()
+                    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'textarea[name="message"]')))
                     text_area = driver.find_element(By.CSS_SELECTOR, 'textarea[name="message"]')
                     text_area.send_keys(message)
 
                 send_btn = driver.find_element(By.CSS_SELECTOR, 'button[aria-label = "Send now"]')
-                send_btn.click()
+                if 'disabled' not in send_btn.get_attribute('class'):
+                    send_btn.click()
+                #TODO: If disabled make a new aswer with chatgpt. Rather than close the window.
+                else:
+                    close_btn = driver.find_element(By.CSS_SELECTOR, 'button[aria-label="Dismiss"]')
+                    close_btn.click()
 
             except:
                 close_btn = driver.find_element(By.CSS_SELECTOR, 'button[aria-label="Dismiss"]')
@@ -74,7 +165,7 @@ def next_page(driver):
     next_btn = driver.find_element(By.CSS_SELECTOR, 'button[aria-label="Next"]')
     next_btn.click()
 
-def connector_pipe_line(url, e_mail, password, PATH, message=False, headless=True, limit=50):
+def connector_pipe_line(login_link, search_link, PATH, message=False, headless=True, limit=50, openai_key="None", my_informations="None"):
     '''
     Function to automates the process of logging in to LinkedIn, connecting with people, and navigating through search results or content pages.
 
@@ -83,25 +174,28 @@ def connector_pipe_line(url, e_mail, password, PATH, message=False, headless=Tru
     url: str (represents the LinkedIn page which consists of people you wish to connect)
     e_mail: str (representing the email or username that you want to input into the login form.)
     password: str (representing the password that you want to input into the login form.)
-    message (optional): srt, optional (Allows you to provide a custom message to send along with the connection request. If not provided, it defaults to False.)
+    message (optional): str, optional (Allows you to provide a custom message to send along with the connection request. If not provided, it defaults to False. If it's defined as chatgpt it will send a message based on the profile of target.)
     headless: boolean (represents driver option. If True opens driver in background)
     limit: int (represents invitation limit. E.g. if it's 50 the script will send 50 invitation)
+    openai_key (optional): str, optional (Needed api key for chatgpt function, needed for sending targeted notes while connecting by using chatgpt)
+    my_informations (optional): str, optional (Your Linkedin profile informations, needed for sending targeted notes while connecting by using chatgpt)
     '''
     # Options for selenium driver
+    service = Service(executable_path=rf'{PATH}')
     op = webdriver.ChromeOptions()
     if headless:
         op.add_argument("--headless")
 
-    linkedin_driver = webdriver.Chrome(PATH, options=op)
-    linkedin_driver.get(url)
-
-    login(linkedin_driver, e_mail, password)
+    linkedin_driver = webdriver.Chrome(service=service, options=op)
+    linkedin_driver.get(login_link)
+    linkedin_driver.get(search_link)
 
     count = 0
 
     while count <= limit:
-        connect(linkedin_driver, message)
+        connect(linkedin_driver, message, openai_key, my_informations)
         count += 10
         next_page(linkedin_driver)
         print("Invitation progress completed!")
-        linkedin_driver.close()
+
+    linkedin_driver.close()
